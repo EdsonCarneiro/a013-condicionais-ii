87e4c75e0fe7b203894832216af39f054ac1aa81
EdsonCarneiro <edsonrje@gmail.com>
2022-08-09 14:53:46 -0300

    ATIVIDADE A013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,22 @@
-const num = //seu numero aqui
+const numero = Number(prompt("Digite um número"))
 
+if (numero % 2 === 0) {
+    if (numero % 3 === 0) {
+        console.log(`Esse numero ${numero} e divisível por 2 ou por 3 `)
+    } else if (!(numero % 3 === 0)) {
+        console.log(`Esse numero ${numero} e divisível por 2 ou por 3 `)
+    }
+} else if (!(numero % 2 === 0)) {
+    if (numero % 3 === 0) {
+        console.log(`Esse numero ${numero} e divisível por 2 ou por 3 `)
+    } else {
+        console.log(`Esse numero ${numero} não e divisível por 2 ou por 3  `)
+    }
+}
+
+
+if(numero%2==0 || numero%3==0){
+    console.log('É divisivel por 2 ou por 3');
+}else {
+    console.log('Não é divisivel por 2 ou por 3');
+}
